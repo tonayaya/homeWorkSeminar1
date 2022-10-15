c885a7d08e85fc20c89063ee838f70aba34f0174
tonayaya <tonayaya@icloud.com>
2022-10-15 20:55:03 +0400

    задача №3

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу, которая на вход принимает число и выдает, является ли число четнымым
+Console.Write("введите любое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число не четное");
+}
