09885ee06647866bcb6e7d608119fc96899f75db
tonayaya <tonayaya@icloud.com>
2022-10-15 21:17:58 +0400

    задача №4

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+int i = 1;
+bool not = true;
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+if (i % 2 != 1)
+{
+Console.Write(i + ", ");
+not = false;
+}
+i++;
+}
+
+if (not)
+{
+Console.WriteLine("Нет чётных чисел!");
+}
