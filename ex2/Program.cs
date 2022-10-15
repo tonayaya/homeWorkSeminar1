df8908f778d6b4416f595c097732f504e37d842a
tonayaya <tonayaya@icloud.com>
2022-10-15 20:29:40 +0400

    задача №2

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из трех чисел.
+Console.Write("введите первое значение: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе значение: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите третье значение: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = Math.Max(numberA,Math.Max(numberB,numberC));
+Console.WriteLine($"максимальное число: {max}");
